c045a0499cc4b4acf9d95a073397142ee79498e9
rutwikrp <rutwikpatil73@gmail.com>
2024-11-21 22:29:18 +0530

    assignment

---
+++ day1_day2/p2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+
+    printf("a & b = %d\n", a & b);  // Bitwise AND
+    printf("a | b = %d\n", a | b);  // Bitwise OR
+    printf("a ^ b = %d\n", a ^ b);  // Bitwise XOR
+    printf("~a = %d\n", ~a);        // Bitwise NOT
+    printf("a << 1 = %d\n", a << 1); // Left shift
+    printf("a >> 1 = %d\n", a >> 1); // Right shift
+
+    return 0;
+}
